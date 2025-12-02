eb51b74c07c1fd41fb14fe585f40cadd8345c84a
spider-kitty <163761719+spider-kitty@users.noreply.github.com>
2025-12-02 21:26:21 +0330

    Add comprehensive views to grading app

--- grading/views.py
+++ grading/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Grading
 
-# Create your views here.
+class GradingListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    """View to list all gradings for teachers only"""
+    model = Grading
+    template_name = 'grading/grading_list.html'
+    context_object_name = 'gradings'
+    paginate_by = 10
+    login_url = 'login'
+    
+    def test_func(self):
+        return hasattr(self.request.user, 'role') and self.request.user.role == 'teacher'
+
+class GradingDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    """View to display a single grading"""
+    model = Grading
+    template_name = 'grading/grading_detail.html'
+    context_object_name = 'grading'
+    login_url = 'login'
+    
+    def test_func(self):
+        return hasattr(self.request.user, 'role') and self.request.user.role == 'teacher'
+
+class GradingUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """View to update grading and provide feedback"""
+    model = Grading
+    template_name = 'grading/grading_form.html'
+    fields = ['marks_awarded', 'feedback']
+    login_url = 'login'
+    success_url = reverse_lazy('grading-list')
+    
+    def test_func(self):
+        return hasattr(self.request.user, 'role') and self.request.user.role == 'teacher'
+    
+    def form_valid(self, form):
+        form.instance.graded_by = self.request.user
+        return super().form_valid(form)
